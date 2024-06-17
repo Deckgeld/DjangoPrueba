d886cba5c3bce40d1c6636306f54745549eceee6
Tu nombre <utm210008ti@ut-morelia.edu.mx>
2024-06-16 18:35:10 -0600

    Update language code and add verbose names to models

--- registros/admin.py
+++ registros/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Alumnos
 
+class AdministrarModelo(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
 # Register your models here.
-admin.site.register(Alumnos)
+admin.site.register(Alumnos, AdministrarModelo)
