31df9edce1eedc77a8d8c8a079c66ce958bcea30
Tu nombre <utm210008ti@ut-morelia.edu.mx>
2024-06-25 09:21:01 -0600

    Update URLs and add ComentarioContacto model and views

--- prueba/urls.py
+++ prueba/urls.py
@@ -23,9 +23,10 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',registros_views.registros, name="Principal"),
-    path('contacto/',views.contacto,name="Contacto"),
+    path('contacto/',registros_views.contacto,name="Contacto"),
     path('formulario/',views.formulario,name="Formulario"),
     path('ejemplo/',views.ejemplo,name="ejemplo"),
+    path('registrar/',registros_views.registrar,name="Registrar"),
 ]
 
 #permite acceder a las variables MEDIA_URL y MEDIA_ROOT que
--- registros/views.py
+++ registros/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from .models import Alumnos
+from .forms import ComentarioContactoForm
 
 # Create your views here.
 def registros(request):
     # se obtienen todos los registros de la tabla Alumnos
     alumnos = Alumnos.objects.all()
     return render(request, 'registros/principal.html', {'alumnos': alumnos})
+
+def registrar(request):
+    if request.method == 'POST':
+        form = ComentarioContactoForm(request.POST)
+        if form.is_valid(): #Si los datos recibidos son correctos
+            form.save() #inserta
+            return render(request,'registros/contacto.html')
+    form = ComentarioContactoForm()
+    #Si algo sale mal se reenvian al formulario los datos ingresados
+    return render(request,'registros/contacto.html',{'form': form})
+
+def contacto(request):
+    return render(request,"registros/contacto.html")
+    #Indicamos el lugar donde se renderizará el resultado de esta vista
