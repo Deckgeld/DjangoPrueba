b8ac392f84f6a16b27cc86165210a8e1fd833a0b
Tu nombre <utm210008ti@ut-morelia.edu.mx>
2024-06-30 19:26:26 -0600

    remove migrations

--- registros/models.py
+++ registros/models.py
@@ -8,6 +8,7 @@ class Alumnos(models.Model):
     nombre = models.TextField() #Texto largo
     carrera = models.TextField()
     turno = models.CharField(max_length=10)
+    #edad = models.IntegerField(null=True)
     imagen = models.ImageField(null=True,upload_to="fotos",verbose_name="Fotografía")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
